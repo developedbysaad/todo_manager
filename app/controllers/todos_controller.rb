9594046903985c52200695804c96bd0a8b2723d9
Mohd Saad <developedbysaad@gmail.com>
2021-07-08 21:42:48 +0530

    User can now complete and update an existing todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,7 +9,8 @@ class TodosController < ApplicationController
   def show
     id = params[:id]
     todo = Todo.find(id)
-    render plain: todo.to_pleasant_string
+    render "todo"
+    #render plain: todo.to_pleasant_string
   end
 
   def create
@@ -29,6 +30,6 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "Updated todo completed status to #{completed}"
+    redirect_to todos_path
   end
 end
