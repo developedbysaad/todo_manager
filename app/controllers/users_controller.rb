81adcc7a5c1b09355c253abb21de0abbf7813a51
Mohd Saad <developedbysaad@gmail.com>
2021-07-11 18:59:05 +0530

    Validate sign-up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,24 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create!(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      password: params[:password],
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    email = params[:email]
+    password = params[:password]
+
+    user = User.new(
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      password: password,
     )
-    session[:current_user_id] = user.id
-    redirect_to "/"
+
+    if user.save
+      session[:current_user_id] = user.id
+      redirect_to todos_path
+    else
+      flash[:error] = user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 end
